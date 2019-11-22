9556885b467eeeff9c2069da1b2008fdffec05bd
chiennv1998 <48088722+chiennv1998@users.noreply.github.com>
2019-11-22 12:22:10 +0700

    Add files via upload

--- app/src/main/java/com/elnino/adm/appnote/MainActivity.java
+++ app/src/main/java/com/elnino/adm/appnote/MainActivity.java
@@ -2,13 +2,27 @@ package com.elnino.adm.appnote;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button them;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        them = findViewById(R.id.them);
+        them.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,ThemGhiChu.class);
+                 startActivity(intent);
+                 finish();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/elnino/adm/appnote/ThemGhiChu.java
@@ -0,0 +1,14 @@
+package com.elnino.adm.appnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemGhiChu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_them_ghi_chu);
+    }
+}
